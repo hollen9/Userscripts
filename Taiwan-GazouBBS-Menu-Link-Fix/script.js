3ed4105adab957e914f458f3f33baacdff8d5da7
hollen9 <hollen.tw@gmail.com>
2018-09-13 22:14:24 +0800

    K2 first commit

---
+++ Taiwan-GazouBBS-Menu-Link-Fix/script.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Taiwan GazouBBS Menu Link Fix
+// @namespace    http://hollen9.yabi.me/userstyle/tawian-gazoubbs-menu-link-fix
+// @version      0.1
+// @description  取消選單追蹤行為，同時也修復了連結導向。目前主要應用在Ｋ２
+// @author       Hollen9
+// @include      *://*komica2.net/bbsmenu*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let oBrokenLinks = document.querySelectorAll("a[onclick^=trackInboundLink]");
+    const regex = /trackInboundLink.*; return false;/gm;
+    oBrokenLinks.forEach(function(o)
+    {
+        let oAttrOnclick = o.getAttribute("onclick");
+        oAttrOnclick = oAttrOnclick.replace(regex, '');
+        o.setAttribute("onclick", oAttrOnclick);
+    });
+    return true;
+})();
